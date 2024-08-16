e64b488ac3e79ed8f0e217218f987af663444710
Karanwar2504 <karan.w@Mygreatlearning.com>
2024-08-16 13:55:37 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,9 +26,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="testml"
+GROUP="DSC"
 LOCATION="eastus"
-WORKSPACE="azureml"
+WORKSPACE="DSC"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
